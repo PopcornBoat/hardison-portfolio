8dab342929618cf077207425c2067efc0c13d96d
yuxuanw <yx703587@dal.ca>
2025-02-12 01:16:58 -0400

    fix 404

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,14 +4,14 @@ const Resume = () => {
             <h2 className="text-center mt-3">My Resume</h2>
             <div className="flex-grow-1 w-100 d-flex justify-content-center">
                 <object
-                    data="/resume/Hardison_resume.pdf"
+                    data="/Hardison_resume.pdf"
                     type="application/pdf"
                     className="w-100"
                     
                 >
                     <p className="text-center mt-3">
                         Your browser does not support PDFs.
-                        <a href="/resume/Hardison_resume.pdf" download>Download the Resume</a> instead.
+                        <a href="/Hardison_resume.pdf" download>Download the Resume</a> instead.
                     </p>
                 </object>
             </div>
