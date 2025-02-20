3990d1f05679644be1306d0f3fa548cf8221af3a
yuxuanw <yx703587@dal.ca>
2025-02-20 15:54:46 -0400

    fix hero typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => (
     <section className="text-center py-20">
-      <h1 className="text-3xl md:text-5xl font-bold">Hi~ I`&apos;`m Hardison! <br />
+      <h1 className="text-3xl md:text-5xl font-bold">Hi~ I&apos;m Hardison! <br />
       Welcome to My Portfolio</h1>
       <p className="text-lg md:text-xl text-gray-600 mt-4">
         Full-Stack Developer | Ethical Hacker | AI Enthusiast | Problem Solver | Researcher
@@ -9,5 +9,5 @@ const Hero = () => (
     </section>
   );
   
-  export default Hero;
+  export default Hero
   
