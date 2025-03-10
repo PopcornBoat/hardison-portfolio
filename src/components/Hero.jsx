3937f63f6f1b3dff30e55c85a2941b9c376bfc59
yuxuanw <yx703587@dal.ca>
2025-03-10 04:05:06 -0300

    I'm a kendoka

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => (
       <h1 className="text-3xl md:text-5xl font-bold">Hi~ I`&apos;`m Hardison! <br />
       Welcome to My Portfolio</h1>
       <p className="text-lg md:text-xl text-gray-600 mt-4">
-        Full-Stack Developer | Ethical Hacker | AI Enthusiast | Problem Solver | Researcher
+        Full-Stack Developer | Ethical Hacker | AI Enthusiast | Problem Solver | Researcher | Kendoka
       </p>
       <img src="profile.jpg" alt="profile-photo" className="profile"></img>
     </section>
